857fc242368c99e5e820521884af2a4aebe6e17d
Jaden Arceneaux <jaden.236829@nv.ccsd.net>
2020-09-23 10:14:07 -0700

    collect data from user

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class ConstructionTester {
     public static void main(String[] args) {
-        Construction myConstruction = new Construction();
+        Scanner scanner = new Scanner();
         
+        System.out.println("What is the tax rate");
+        double taxRate = scanner.nextDouble();
+
+        System.out.println("How many boards do you need");
+        int boards = scanner.nextInt();
+        
+        System.out.println("How many windows do you need");
+        int windows = scanner.nextInt();
+        
+        Construction myConstruction = new Construction();
+
     }
 }
