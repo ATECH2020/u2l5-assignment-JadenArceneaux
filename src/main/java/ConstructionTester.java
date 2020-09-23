e5b79925573b18e499a05b5c1331e69768028823
Jaden Arceneaux <jaden.236829@nv.ccsd.net>
2020-09-23 10:18:52 -0700

    fixed system in

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ConstructionTester {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
         
         System.out.println("What is the tax rate");
         double taxRate = scanner.nextDouble();
