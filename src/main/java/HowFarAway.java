18e687fec3324a1671e40ebf709891baf5491fb1
Jaden Arceneaux <jaden.236829@nv.ccsd.net>
2020-09-27 22:01:31 -0700

    comments

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -2,24 +2,26 @@ import java.util.Scanner;
 
 public class HowFarAway {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // declare new scanner
 
+        // Get starting location
         System.out.print("Enter the latitude of the starting location: ");
         double startLatitude = scanner.nextDouble();
         System.out.print("Enter the longitude of the starting location: ");
         double startLongitude = scanner.nextDouble();
 
+        // Get ending location
         System.out.print("Enter the latitude of the ending location: ");
         double endLatitude = scanner.nextDouble();
         System.out.print("Enter the longitude of the ending location: ");
         double endLongitude = scanner.nextDouble();
 
-        scanner.close();
+        scanner.close(); // close scanner
 
-        GeoLocation myGL1 = new GeoLocation(startLatitude, startLongitude);
-        GeoLocation myGL2 = new GeoLocation(endLatitude, endLongitude);
+        GeoLocation myGL1 = new GeoLocation(startLatitude, startLongitude); // create location 1
+        GeoLocation myGL2 = new GeoLocation(endLatitude, endLongitude); // create location 2
 
-        System.out.println("The distance is " + myGL1.distanceFrom(myGL2));
+        System.out.println("The distance is " + myGL1.distanceFrom(myGL2)); // output distance
 
     }
 }
