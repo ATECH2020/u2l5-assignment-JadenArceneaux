9f8d84ef6623bf3f73d893f009ab90918adc7f07
Jaden Arceneaux <jaden.236829@nv.ccsd.net>
2020-09-27 21:54:18 -0700

    user input

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
-public class HowFarAway 
-{
-    public static void main(String[] args)
-    {
-        // your code here.
+public class HowFarAway {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the latitude of the starting location: ");
+        double startLatitude = scanner.nextDouble();
+        System.out.print("Enter the longitude of the starting location: ");
+        double startLongitude = scanner.nextDouble();
+
+        System.out.print("Enter the latitude of the ending location: ");
+        double endLatitude = scanner.nextDouble();
+        System.out.print("Enter the longitude of the ending location: ");
+        double endLongitude = scanner.nextDouble();
+
     }
 }
